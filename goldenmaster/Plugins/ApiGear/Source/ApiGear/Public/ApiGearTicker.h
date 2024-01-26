89087d14a60119a4ca5fafb904cf99c06abf810c
dphan10 <dorota.phan@siili.com>
2024-01-26 11:04:51 +0100

    feat: add aliases for version dependent ticker
    add ApiGear version of ticker and delegate handle so they can by used
    in a code without macros for checking the version of unreal engine

---
+++ goldenmaster/Plugins/ApiGear/Source/ApiGear/Public/ApiGearTicker.h
@@ -0,0 +1,30 @@
+// Copyright Epic Games, Inc. All Rights Reserved
+
+#pragma once
+#include "Runtime/Launch/Resources/Version.h"
+#include "Containers/Ticker.h"
+
+namespace ApiGear
+{
+#if (ENGINE_MAJOR_VERSION >= 5)
+using FDelegateHandle = FTSTicker::FDelegateHandle;
+#else
+using FDelegateHandle = ::FDelegateHandle;
+#endif
+} // namespace ApiGear
+
+class ApiGearTicker
+{
+public:
+#if (ENGINE_MAJOR_VERSION >= 5)
+	static FTSTicker& GetCoreTicker()
+	{
+		return FTSTicker::GetCoreTicker();
+	}
+#else
+	static FTicker& ApiGearTicker::GetCoreTicker()
+	{
+		return GetCoreTicker();
+	}
+#endif
+};
